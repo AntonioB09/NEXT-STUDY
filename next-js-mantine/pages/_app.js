56df4ea90b25d26674d87ac2d0bd48391259cb32
AntoniobB09 <antblanco@gmail.com>
2022-08-07 13:31:29 -0400

    init mantine

---
+++ next-js-mantine/pages/_app.js
@@ -0,0 +1,32 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
+import { ApplitationContainer} from '../components/ApplitationContainer';
+import '../styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+ 
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <ApplitationContainer>
+        <Component {...pageProps} />
+        </ApplitationContainer>
+
+      </MantineProvider>
+    </>
+  );
+}
