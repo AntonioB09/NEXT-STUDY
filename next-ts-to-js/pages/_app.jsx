293f329d3a11be91442bb8fd5cefeb3bac4a83f6
AntoniobB09 <antblanco@gmail.com>
2022-08-07 01:07:44 -0400

    navbar-footer-color

--- next-ts-to-js/pages/_app.jsx
+++ next-ts-to-js/pages/_app.jsx
@@ -1,7 +1,16 @@
+
+import Layout from '../components/layout'
 import '../styles/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  
+  return (
+    <Layout >
+      <Component {...pageProps} />
+    </Layout>
+  )
+
 }
 
 export default MyApp
--- next-ts-to-js/tailwind.config.js
+++ next-ts-to-js/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [ 
   "./node_modules/flowbite-react/**/*.js",   
@@ -6,7 +7,23 @@ module.exports = {
   "./components/**/*.{js,ts,jsx,tsx}",
 ],
   theme: {
-    extend: {},
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      Yellow: colors.amber,
+      Green:colors.emerald
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      '128': '32rem',
+      '144': '36rem',
+    },
+    borderRadius: {
+      '4xl': '2rem', 
+    },
   },
   plugins: [require("flowbite/plugin")
 ],
